e1fb8af0da1a9df33d62155345c206547b4df8eb
Alisson Sanches <alissonsanches54@gmail.com>
2026-01-06 21:21:25 -0300

    Feat: writing

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const cPas = document.querySelector("#password");
 const cCop = document.querySelector("#copy");
+const cGer = document.querySelector("#generate");
 
 const nCon = document.querySelector("#con");
 const cMai = document.querySelector("#mai");
@@ -9,15 +10,77 @@ const cEsp = document.querySelector("#esp");
 
 nCon.value = 0;
 
+const alf = [
+    'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s',
+    't','u','w','x','y','z'
+];
+
+const esp = [
+    '!', '@', '#', '$', '%', '^', '&', '*',
+    '(', ')', '-', '_', '=', '+',
+    '[', ']', '{', '}',
+    ';', ':', "'", '"',
+    ',', '.', '<', '>', '/',
+    '?', '\\', '|', '`', '~'
+];
+
 const copy = async () => {
     await navigator.clipboard.writeText(cPas.value);
 }
 
 const schema = (string) => {
-    for (let i = 0; i < string.length; i++) {
-        if (string[i] === '') {
+    let isMin = false;
+    let isMai = false;
+    let isNum = false;
+    let isEsp = false;
+    cMai.checked = false;
+    cMin.checked = false; 
+    cNum.checked = false;
+    cEsp.checked = false; 
 
+    for (let i = 0; i < string.length; i++) {
+        if (string[i] === ' ') {
+            throw new Error('Não pode conter espaços vazios!');
         }
+
+        for (let x = 0; x < alf.length; x++) {
+            if (string[i] === alf[x]) {
+                isMin = true;
+                break;
+            }
+
+            if (string[i] === alf[x].toUpperCase()) {
+                isMai = true;
+                break;
+            }
+
+            for (let y = 0; y < 10; y++) {
+                if (Number(string[i]) === y) {
+                    isNum = true;
+                    break;
+                }
+            }
+
+            for (let z = 0; z < esp.length; z++) {
+                if (string[i] === esp[z]) {
+                    isEsp = true;
+                    break;
+                }
+            }
+        }
+    }
+
+    if (isMai) {
+        cMai.checked = true;
+    }
+    if (isMin) {
+        cMin.checked = true;
+    }
+    if (isNum) {
+        cNum.checked = true;
+    }
+    if (isEsp) {
+        cEsp.checked = true;
     }
 }
 
@@ -25,8 +88,28 @@ const valid = (e) => {
     let con = e.length;
     nCon.value = con;
 
-    schema(e);
+    try {
+        schema(e);
+    } catch (error) {
+        // Mostra na tela se quiser...
+    }
+}
+
+const generate = () => {
+   if (cMai.checked) {
+
+   }
+   if (cMin.checked) {
+
+   }
+   if (cNum.checked) {
+
+   }
+   if (cEsp.checked) {
+
+   } 
 }
 
 cPas.addEventListener('input', (e) => { valid(e.target.value) });
 cCop.addEventListener('click', copy);
+cGer.addEventListener('click', generate);
